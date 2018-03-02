3cec5c9bae9e49b75b1d8225fdec85b4e7957f86
James Boggs <boggsj@crochet-01.union.edu>
2018-03-01 20:20:31 -0500

    Refactors blockchain.py for better formatting
    I moved the docstrings to their proper places and modifeid them
    to conform to the proper format (and also English spelling and
    grammar).
    
    I also added a couple instance variables to Blockchain which we'll
    probably need later.

--- blockchain.py
+++ blockchain.py
@@ -1,101 +1,127 @@
 import queue
 import threading
 import logging
-import traceback, sys
+import traceback
+import sys
+
 from blockchain_constants import *
+
 
 class Blockchain:
 
-    '''Responsible for initializing a block chain object.  It currently
-    takes no parameters, and does nothing, you'll want to change that.
-    This object is created by blockchain_bbs.py.
-
-    '''
     def __init__(self):
+        """
+        Responsible for initializing a Block object.
+
+        The Blockchain object is responsible for keeping track of the state of
+        the Blockchain as seen from the comptuer it is being run on as well as
+        handling interactions with other peers on the blockchain network.
+
+        blockchain_bbs.py creates a Blockchain object which it then passes to
+        a Server object from network.py
+        """
+        self.chain = []  # list of all valid blocks known to this chain.
+        self.latest_block = None  # latest block mined by this blockchain.
+        self.message_queue = queue.Queue()
+        # TODO Define what a Blockchain object looks like
 
 
-        # Use this lock to protect internal data of this class from
-        # the multi-threaded server.  Wrap code modifies the
-        # blockchain in the context "with self.lock:".  Be careful not
-        # to nest these contexts or it will cause deadlock.
+        # Use this lock to protect internal data of this class from the
+        # multi-threaded server.  Wrap code which modifies the blockchain with
+        # "with self.lock:". Be careful not to nest these contexts or it will
+        # cause deadlock.
         self.lock = threading.Lock()
-        
+
 
         pass
-        
-    '''Returns the number of messages that are queued to be processed by
-    the blockchain.  This function is called by networking.py.
 
-    '''
     def get_message_queue_size(self):
-        return 0
-        
-        
-    '''Takes a string containing a message received by the server from a
-    peer or a user.  The message may be illformed, a duplicate, or
-    invalid.  If this is not the case, add it message to the queue of
-    messages to be processed into the blockchain, and return True.
-    Otherwise, return False.  This function is called by
-    networking.py.
+        """
+        Get the number of messages that are queued to be processed.
 
-    '''
+        This function returns the number of messages which the Blockchain
+        object has received which have yet to be processed.
+
+        This function is called by networking.py.
+        """
+        return 0
+
     def add_message_str(self, msg_str):
+        """
+        Verifies then adds incoming messages to the message queue.
+
+        This method takes a string containing a message received by the server
+        from a peer or a user.  The message may be illformed, a duplicate, or
+        invalid.  It first verifies that this is not the case, then adds the
+        message to the queue of messages to be processed into the blockchain,
+        and returns True. If the message is invalid, this method returns False.
+
+        This function is called by networking.py.
+        """
 
         #print(msg_str)
-        
+
         return False
 
-    
-    '''Takes a string containing a block received by the server from a
-    peer or a user.  The block may be illformed, invalid, refer to
-    non-existant previous block, or be a duplicate.  If this is not
-    the case, add it to the blockchain and return True.  (This node's
-    mining may be interrupted, if this new block supercedes the
-    current block.)  Otherwise, return False.  This function is called
-    by networking.py.
-
-    '''
     def add_block_str(self, block_str):
+        """
+        Verifies then adds incoming blocks to the blockchain.
+
+        This method takes a string containing a block received by the server
+        from a peer or a user.  The block may be illformed, a duplicate, or
+        invalid.  It first verifies that this is not the case, then adds the
+        block to the blockchain and returns True. If the block is invalid, this
+        method returns False.
+
+        This function is called by networking.py.
+        """
 
         #print(block_str)
-        
+
         return False
 
-
-    '''Return the string encoding of a newly mined block if it exists, and
-    otherwise returns None.  This function should return immediately
-    and not wait for a block to be mined.  This function is called by
-    networking.py.
-
-    '''
     def get_new_block_str(self):
+        """
+        Get the latest mined block if one exists.
+
+        This method returns the string encoding of a newly mined block if it
+        such a block exists, and otherwise returns None. This is a NON-BLOCKING
+        function, it returns immediately without waiting for a new block to be
+        mined.
+
+        This function is called by networking.py.
+        """
 
         return None
 
-
-    '''Returns a list of the string encoding of each of the blocks in this
-    blockchain, including ones not on the main chain, whose timestamp
-    is greater then the parameter t.  This function is called by
-    networking.py.
-
-    '''
     def get_all_block_strs(self, t):
+        """
+        Get the string representation of every block in the chain after time t.
+
+        This method returns a list of the string encoding of each of the blocks
+        in this blockchain, including ones not on the main chain, whose
+        timestamp is greater then the parameter t.
+
+        This function is called by networking.py.
+        """
 
         return []
 
-
-    '''Waits for enough messages to be received from the server, forms
-    them into blocks, mines the block, adds the block to the
-    blockchain, and prepares the block to be broadcast by the server.
-    The mining of a block may be interrupted by a superceding
-    add_block_str() call.  In this case the miner should does its best
-    to move on to mine another block and not lose any messages it was
-    previous attempting to mine.  This process repeats forever, and
-    this function never runs.  This function is called in
-    blockchain_bbs.py as a new thread.
-
-    '''
     def mine(self):
+        """
+        Mine a new block featuring the latest incoming messages.
+
+        This method waits for enough messages to be received from the server,
+        then forms them into blocks, mines the block, adds the block to the
+        blockchain, and prepares the block to be broadcast by the server.
+        The mining of a block may be interrupted by a superceding
+        add_block_str() call.  In this case the miner should do its best to
+        move on to mine another block and not lose any messages it was
+        previously attempting to mine.  This process repeats forever, and this
+        function never runs.
+
+        This function is called in blockchain_bbs.py as a new thread.
+        """
 
         while True:
             pass
