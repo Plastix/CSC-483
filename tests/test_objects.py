4f0d710dba44fdf7a6d49ea3eacc9665ea5a3f23
Plastix <aidanpieper@gmail.com>
2018-03-04 12:26:58 -0500

    Add unit tests for Message parsing

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,33 +1,53 @@
 import unittest
 
-from objects import parse_block
-
-parse_block_tests = [
-    ["parse_valid_block", "valid_block.txt", True],
-    ["parse_random", "random.txt", False],
-    ["parse_empty", "empty.txt", False],
-    ["parse_empty", "empty.txt", False],
-    ["parse_empty_nonce", "empty_nonce.txt", False],
-    ["parse_bad_nonce", "bad_nonce.txt", False],
-    ["parse_empty_parent_hash", "empty_parent_hash.txt", False],
-    ["parse_bad_parent_hash", "bad_parent_hash.txt", False],
-    ["parse_no_messages", "no_messages.txt", False],
-    ["parse_empty_messages", "empty_messages.txt", False],
-    ["parse_empty_miner_id", "empty_miner_id.txt", False],
-    ["parse_bad_miner_id", "bad_miner_id.txt", False],
-    ["parse_empty_timestamp", "empty_timestamp.txt", False],
-    ["parse_bad_timestamp", "bad_timestamp.txt", False],
-]
+from objects import parse_block, parse_message
 
 
-class TestParseMethods(unittest.TestCase):
+class TestBlockParsing(unittest.TestCase):
+    block_data_path = 'tests/block_data/'
+    block_tests = [
+        ["parse_valid_block", "valid_block.txt", True],
+        ["parse_random", "random.txt", False],
+        ["parse_empty", "empty.txt", False],
+        ["parse_empty_nonce", "empty_nonce.txt", False],
+        ["parse_bad_nonce", "bad_nonce.txt", False],
+        ["parse_empty_parent_hash", "empty_parent_hash.txt", False],
+        ["parse_bad_parent_hash", "bad_parent_hash.txt", False],
+        ["parse_no_messages", "no_messages.txt", False],
+        ["parse_empty_messages", "empty_messages.txt", False],
+        ["parse_empty_miner_id", "empty_miner_id.txt", False],
+        ["parse_bad_miner_id", "bad_miner_id.txt", False],
+        ["parse_empty_timestamp", "empty_timestamp.txt", False],
+        ["parse_bad_timestamp", "bad_timestamp.txt", False],
+    ]
+
+    @staticmethod
+    def generate_block_test(file, result):
+        def test(self):
+            with open(TestBlockParsing.block_data_path + file, 'r') as data:
+                string = data.read()
+                block = parse_block(string)
+                if result:
+                    self.assertTrue(block)
+                else:
+                    self.assertFalse(block)
+
+        return test
+
+    @staticmethod
+    def setup_tests():
+        for test in TestBlockParsing.block_tests:
+            test_name = 'test_%s' % test[0]
+            func = TestBlockParsing.generate_block_test(test[1], test[2])
+            setattr(TestBlockParsing, test_name, func)
+
     def test_to_string(self):
-        with open('tests/data/valid_block.txt', 'r') as data:
+        with open(TestBlockParsing.block_data_path + 'valid_block.txt', 'r') as data:
             string = data.read()
             self.assertEquals(repr(parse_block(string)), string)
 
     def test_parsing_data(self):
-        with open('tests/data/valid_block.txt', 'r') as data:
+        with open(TestBlockParsing.block_data_path + 'valid_block.txt', 'r') as data:
             block = parse_block(data.read())
             print(block)
             self.assertEquals(block.nonce, 280815619281315998298257152433917011173307790385)
@@ -37,29 +57,53 @@ class TestParseMethods(unittest.TestCase):
             # TODO Verify messages are correct
 
 
-def generate_block_test(file, result):
-    def test(self):
-        if result:
-            self.assertTrue(parse(file))
-        else:
-            self.assertFalse(parse(file))
+class TestMessageParsing(unittest.TestCase):
+    message_data_path = 'tests/message_data/'
+    message_tests = [
+        ["parse_valid_public", "valid_public.txt", True],
+        ["parse_random", "random.txt", False],
+        ["parse_empty", "empty.txt", False],
+        ["parse_empty_public_key", "empty_public_key.txt", False],
+        ["parse_bad_public_key", "bad_public_key.txt", False],
+        ["parse_empty_ds", "empty_ds.txt", False],
+        ["parse_bad_ds", "bad_ds.txt", False],
+        ["parse_empty_timestamp", "empty_timestamp.txt", False],
+        ["parse_bad_timestamp", "bad_timestamp.txt", False],
+        ["parse_empty_message_string", "empty_message_string.txt", True],
+        ["parse_bad_message_string", "bad_message_string.txt", False],
+    ]
 
-    return test
+    @staticmethod
+    def generate_message_test(file, result):
+        def test(self):
+            with open(TestMessageParsing.message_data_path + file, 'r') as data:
+                string = data.read()
+                block = parse_message(string)
+                if result:
+                    self.assertTrue(block)
+                else:
+                    self.assertFalse(block)
+
+        return test
+
+    @staticmethod
+    def setup_tests():
+        for test in TestMessageParsing.message_tests:
+            test_name = 'test_%s' % test[0]
+            func = TestMessageParsing.generate_message_test(test[1], test[2])
+            setattr(TestMessageParsing, test_name, func)
+
+    def test_to_string(self):
+        with open(TestMessageParsing.message_data_path + 'valid_public.txt', 'r') as data:
+            string = data.read()
+            self.assertEquals(repr(parse_message(string)), string)
+
+    def test_parsing_data(self):
+        # TODO write this
+        self.fail()
 
 
-def setup_tests():
-    for test in parse_block_tests:
-        test_name = 'test_%s' % test[0]
-        func = generate_block_test(test[1], test[2])
-        setattr(TestParseMethods, test_name, func)
-
-
-def parse(file):
-    with open('tests/data/' + file, 'r') as file:
-        data = file.read()
-        return parse_block(data)
-
-
-setup_tests()
+TestBlockParsing.setup_tests()
+TestMessageParsing.setup_tests()
 if __name__ == '__main__':
     unittest.main()
