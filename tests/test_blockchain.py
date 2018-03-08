07ebbaeb8b660386f832c8693acbb841efbd1a6a
Plastix <aidanpieper@gmail.com>
2018-03-08 16:14:57 -0500

    Add more unit tests for blockchain.py methods

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -6,6 +6,9 @@ from blockchain import Blockchain
 
 class TestBlockchain(unittest.TestCase):
     valid_chain = 'tests/chain_data/valid_chain.txt'
+    valid_message = 'tests/message_data/valid_public.txt'
+    invalid_message = 'tests/message_data/bad_timestamp.txt'
+    invalid_ds = 'tests/message_data/bad_ds.txt'
 
     def setUp(self):
         logging.getLogger("blockchain").disabled = True
@@ -14,6 +17,30 @@ class TestBlockchain(unittest.TestCase):
     def test_add_block_str(self):
         self.add_all_blocks(TestBlockchain.valid_chain)
         self.assertEqual(len(self.blockchain.get_all_block_strs(0)), 5)
+
+    def test_message_queue_empty(self):
+        self.assertEqual(self.blockchain.get_message_queue_size(), 0)
+
+    def test_message_queue_single_message(self):
+        self.blockchain.add_message_str(get_message_str(TestBlockchain.valid_message))
+        self.assertEqual(self.blockchain.get_message_queue_size(), 1)
+
+    def test_message_queue_no_duplicates(self):
+        msg = get_message_str(TestBlockchain.valid_message)
+        self.blockchain.add_message_str(msg)
+        self.blockchain.add_message_str(msg)
+        self.assertEqual(self.blockchain.get_message_queue_size(), 1)
+
+    def test_message_queue_invalid_message(self):
+        self.blockchain.add_message_str(get_message_str(TestBlockchain.invalid_message))
+        self.assertEqual(self.blockchain.get_message_queue_size(), 0)
+
+    def test_message_queue_wrong_ds(self):
+        self.blockchain.add_message_str(get_message_str(TestBlockchain.invalid_ds))
+        self.assertEqual(self.blockchain.get_message_queue_size(), 0)
+
+    def test_no_mined_block(self):
+        self.assertIsNone(self.blockchain.get_new_block_str())
 
     def test_get_all_block_strs(self):
         """
@@ -35,6 +62,11 @@ class TestBlockchain(unittest.TestCase):
             for block_string in blocks:
                 self.blockchain.add_block_str(block_string)
             return raw_data
+
+
+def get_message_str(file):
+    with open(file, 'r') as message:
+        return message.read()
 
 
 if __name__ == '__main__':
